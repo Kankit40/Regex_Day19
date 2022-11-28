735288267704d1f34eb732fb589e7a69c6f8bb38
Kankit40 <Kankit40@gmail.com>
2022-11-28 17:53:05 +0530

    Welcome to Regex Validator

---
+++ src/com/bridgelabz/RegexExpression.java
@@ -0,0 +1,30 @@
+package com.bridgelabz;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexExpression {
+    public static boolean checkValidName(String firstName) {
+        String firstNameRegex= "^[A-Z]{1}[A-Za-z]{2,}$";
+        Pattern obj= Pattern.compile(firstNameRegex);
+
+        if(firstName==null) {
+            return false;
+
+        }
+        Matcher obj2 = obj.matcher(firstName);
+        return obj2.matches();
+    }
+    public static void main(String[] args) {
+
+        System.out.println("----- Welcome To Name Validator -----");
+
+        String firstName ="Ankit";
+        boolean isfirstName = checkValidName(firstName);
+
+        if(isfirstName)
+            System.out.println(firstName+" is an Valid firstName");
+        else
+            System.out.println(firstName+" is an Invalid firstName");
+    }
+}
